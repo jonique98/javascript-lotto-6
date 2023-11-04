c894438402413f74506a21b1b0f17941c0f8e8c4
jonique98 <tnals9899@naver.com>
2023-11-04 20:41:24 +0900

    feat: validate check

--- src/Lotto.js
+++ src/Lotto.js
@@ -7,8 +7,39 @@ class Lotto {
   }
 
   #validate(numbers) {
+    this.#numberLengthCheck(numbers);
+    this.#numberTypeCheck(numbers);
+    this.#numberRangeCheck(numbers);
+    this.#numberDuplicateCheck(numbers);
+  }
+
+  #numberLengthCheck(numbers) {
     if (numbers.length !== 6) {
       throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
+    }
+  }
+
+  #numberTypeCheck(numbers) {
+    numbers.every((number) => {
+      if (Number.isNAN(number)) {
+        throw new Error("[ERROR] 로또 번호는 정수여야 합니다.");
+      }
+    });
+  }
+
+  #numberRangeCheck(numbers) {
+    numbers.every((number) => {
+      if (number < 1 || number > 45) {
+        throw new Error("[ERROR] 로또 번호는 1~45 사이여야 합니다.");
+      }
+    });
+  }
+
+
+  #numberDuplicateCheck(numbers) {
+    const duplicateChecker = new Set(numbers);
+    if (duplicateChecker.size !== 6) {
+      throw new Error("[ERROR] 로또 번호에 중복된 숫자가 있습니다.");
     }
   }
 
